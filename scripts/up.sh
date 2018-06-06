0e4f93291b7e7af1073e021b3e170ee363366913
Caleb <caleb.falcione@gmail.com>
2018-06-06 15:13:41 -0500

    adds error handling to up.sh

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose up -d rc-rabbitmq
+docker-compose up -d rc-rabbitmq || exit 1
 echo Waiting for rabbitmq...
 while ! curl -s http://localhost:15672 >> /dev/null ; do sleep 3; done
 
