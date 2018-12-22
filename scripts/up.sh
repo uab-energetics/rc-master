7ba63bf5c368af18b286fdeb7db7c8bccdb1f842
Caleb <caleb.falcione@gmail.com>
2018-12-22 17:25:54 -0600

    updates documentation

--- scripts/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker-compose up -d rc-rabbitmq || exit 1
-echo Waiting for rabbitmq...
-while ! curl -s http://localhost:15672 >> /dev/null ; do sleep 3; done
-
-echo Starting the remainder of the services
-docker-compose up $@
